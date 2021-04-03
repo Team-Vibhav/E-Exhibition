5f9e5375240c1f8869fecff5723630067941bccf
vishal9629 <62130870+vishal9629@users.noreply.github.com>
2021-04-03 23:59:46 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request ,jsonify
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+model_name = "deepset/roberta-base-squad2"
+nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
+
+MAX_LEN = 320
+
+app = Flask(__name__)
+
+@app.route("/")
+def index_page():
+    return "Question Answering API for E-Exhibition"
+
+@app.route("/predict", methods=["POST", "GET"])
+def predict():
+    if request.method == "POST":
+         ques = request.form.get("question")
+         context = request.form.get("context")
+        
+         QA_input = {
+         'question': ques,
+         'context': context
+         }    
+         prediction = nlp(QA_input)
+         if prediction['score']>0.18:
+            return jsonify(prediction)
+         else:
+            return "Change The Question Please." 
+    return "MODEL FAILED"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
