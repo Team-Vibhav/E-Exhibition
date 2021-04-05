92af6b4555a75508d33bbf5d4c58a0483489e167
vishal9629 <62130870+vishal9629@users.noreply.github.com>
2021-04-06 01:25:29 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request ,jsonify
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+import cont
+
 model_name = "deepset/roberta-base-squad2"
 nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
 
@@ -15,7 +17,8 @@ def index_page():
 def predict():
     if request.method == "POST":
          ques = request.form.get("question")
-         context = request.form.get("context")
+         contextqna = request.form.get("contextqna")
+         context = cont.CONTEXT[contextqna]
         
          QA_input = {
          'question': ques,
@@ -27,6 +30,24 @@ def predict():
          else:
             return "Change The Question Please." 
     return "MODEL FAILED"
+
+@app.route("/add_context",methods=["POST"])
+def add_context():
+    if request.method=="POST":
+        context_no = request.form.get("context_no")
+        new_context = request.form.get("new_context")
+
+        #cont.CONTEXT[context_no] = new_context
+        cont.CONTEXT.update({context_no:new_context})
+        print(cont.CONTEXT)
+
+        return "Context Changed Successfully"
+    return "Unsuccessful Change"
+
+@app.route("/show_contexts",methods=["GET"])
+def show_contexts():
+    return cont.CONTEXT
+
 
 
 if __name__ == "__main__":
